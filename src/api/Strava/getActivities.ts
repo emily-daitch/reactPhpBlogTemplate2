69764b7c229a047e448342fbcf1de7f3c803c506
Emily Daitch <emily.daitch@gmail.com>
2023-08-03 16:36:50 -0400

    use actual backend for strava data

--- src/api/Strava/getActivities.ts
+++ src/api/Strava/getActivities.ts
@@ -1,14 +1,19 @@
 import { SummaryActivity } from '../../types/strava';
 
+const env = process.env.STAGE;
+const url = process.env.URL;
+const certed = process.env.CERTED;
+const protocol = certed === 'false' ? 'http' : 'https';
+
 export const getActivities = async ():Promise<[SummaryActivity]> => {
-    const activities = await fetch('http://localhost/reactPhpBlogTemplate2/api/getStravaActivities');
+    const activities = await fetch(`${protocol}://${env}${url}/api/getStravaActivities`);
     const data = await activities.json();
     console.log('data: ', data);
     return data;
 }; 
 
 export const getMapActivity = async ():Promise<SummaryActivity> => {
-    const activities = await fetch('http://localhost/reactPhpBlogTemplate2/api/getMapActivity');
+    const activities = await fetch(`${protocol}://${env}${url}/api/getMapActivity`);
     const data = await activities.json();
     console.log('map data: ', data);
     return data;
