a27ab253c4c52693faeadb103d30f1a9316fcad8
Emily Daitch <emily.daitch@gmail.com>
2023-08-08 13:57:09 -0400

    Add swim data to weekly graph and toggle options

--- src/components/Pages/Strava.tsx
+++ src/components/Pages/Strava.tsx
@@ -25,9 +25,11 @@ export const getDailyTotals = (dailyTotalData: SummaryActivity[][], dateArray: s
     const walkData = dailyTotalData[0];
     const runData = dailyTotalData[1];
     const bikeData = dailyTotalData[2];
+    const swimData = dailyTotalData[3];
     const walkGraphData: any[] = [];
     const runGraphData: any[] = [];
     const bikeGraphData: any[] = [];
+    const swimGraphData: any[] = [];
     for(const date of dateArray){
         walkGraphData.push({
             start_date: date,
@@ -71,9 +73,23 @@ export const getDailyTotals = (dailyTotalData: SummaryActivity[][], dateArray: s
                 (accumulator, currentValue) => accumulator + currentValue.distance,
                 0.0),
         });
+        swimGraphData.push({
+            start_date: date,
+            label: date,
+            distance: swimData.filter((datum: SummaryActivity) => {
+                return datum.start_date === date ? true : false;
+            }).reduce(
+                (accumulator, currentValue) => accumulator + currentValue.distance,
+                0),
+            y: swimData.filter((datum: SummaryActivity) => {
+                return datum.start_date === date ? true : false;
+            }).reduce(
+                (accumulator, currentValue) => accumulator + currentValue.distance,
+                0.0),
+        });
     }
     console.log('bikeGraphDatat', bikeGraphData);
-    return [walkGraphData, runGraphData, bikeGraphData];
+    return [walkGraphData, runGraphData, bikeGraphData, swimGraphData];
 };
 
 export default function Strava() {
@@ -84,12 +100,38 @@ export default function Strava() {
     const [mapError, setMapError] = useState(null);
     const [stravaLoading, setStravaLoading] = useState(true);
     const [mapLoading, setMapLoading] = useState(true);
+
+    const [walksChecked, setWalksChecked] = React.useState(true);
+    const handleWalksChecked = () => {
+      setWalksChecked(!walksChecked);
+    };
+
+    const [runsChecked, setRunsChecked] = React.useState(true);
+    const handleRunsChecked = () => {
+      setRunsChecked(!runsChecked);
+    };
+
+    const [bikesChecked, setBikesChecked] = React.useState(true);
+    const handleBikesChecked = () => {
+      setBikesChecked(!bikesChecked);
+    };
+
+    const [swimsChecked, setSwimsChecked] = React.useState(true);
+    const handleSwimsChecked = () => {
+      setSwimsChecked(!swimsChecked);
+    };
+
     async function getActivity() {
         try {
             setStravaLoading(true);
             const stravaData = await getActivities();
+            let convertedArray = stravaData.map((datum: SummaryActivity) => {
+                datum.distance = datum.distance * 0.000621371;
+                datum.average_speed = datum.average_speed * 2.23694;
+                return datum;
+            });
             const mapData = await getMapActivity();
-            setStravaData(stravaData);
+            setStravaData(convertedArray);
             setMapData(mapData);
         } catch (e: any) {
             setStravaError(e);
@@ -135,8 +177,6 @@ export default function Strava() {
     ];
     
     const parsedStravaData = stravaData.filter((datum: SummaryActivity) => {
-        datum.distance = datum.distance * 0.000621371;
-        datum.average_speed = datum.average_speed * 2.23694;
         datum.start_date = datum.start_date.slice(0, 10);
         return dateArray.includes(datum.start_date) ? true : false;
     });
@@ -150,14 +190,18 @@ export default function Strava() {
     const bikeData = parsedStravaData.filter((datum: SummaryActivity) => {
         return datum.sport_type === 'Ride' ? true : false;
     });
+    const swimData = parsedStravaData.filter((datum: SummaryActivity) => {
+        return datum.sport_type === 'Swim' ? true : false;
+    });
 
-    const dailyTotalData = [walkData, runData, bikeData];
+    const dailyTotalData = [walkData, runData, bikeData, swimData];
     console.log('test b4 get daily');
 
     const dd: DateDistance[][] = getDailyTotals(dailyTotalData, dateArray);
     const walkGraphData = dd[0];
     const runGraphData = dd[1];
     const bikeGraphData = dd[2];
+    const swimGraphData = dd[3];
     
     const options = {
         animationEnabled: true,
@@ -190,27 +234,34 @@ export default function Strava() {
             reversed: true,
         },
         data: [
-            {
+            runsChecked ? {
                 type: 'stackedColumn',
                 name: 'Runs',
                 showInLegend: true,
                 yValueFormatString: '##.# mi.',
                 dataPoints: runGraphData
-            },
-            {
+            } : {},
+            walksChecked ? {
                 type: 'stackedColumn',
                 name: 'Walks',
                 showInLegend: true,
                 yValueFormatString: '##.# mi.',
                 dataPoints: walkGraphData
-            },
-            {
+            } : {},
+            bikesChecked ? {
                 type: 'stackedColumn',
                 name: 'Bikes',
                 showInLegend: true,
                 yValueFormatString: '##.# mi.',
                 dataPoints: bikeGraphData
-            },
+            } : {},
+            swimsChecked ? {
+                type: 'stackedColumn',
+                name: 'Swims',
+                showInLegend: true,
+                yValueFormatString: '##.# mi.',
+                dataPoints: swimGraphData
+            } : {}
         ]
     };
 
@@ -337,6 +388,38 @@ export default function Strava() {
             See the Strava API that provides this data <Link to='https://developers.strava.com/' style={{color: '#345678'}} target='_blank' rel='noopener noreferrer'>here</Link>.
             <br/><br/>
             Last Week of Activity:
+            <label>
+                <input
+                    type="checkbox"
+                    checked={walksChecked}
+                    onChange={handleWalksChecked}
+                />
+                Walks
+            </label>
+            <label>
+                <input
+                    type="checkbox"
+                    checked={runsChecked}
+                    onChange={handleRunsChecked}
+                />
+                Runs
+            </label>
+            <label>
+                <input
+                    type="checkbox"
+                    checked={bikesChecked}
+                    onChange={handleBikesChecked}
+                />
+                Bikes
+            </label>
+            <label>
+                <input
+                    type="checkbox"
+                    checked={swimsChecked}
+                    onChange={handleSwimsChecked}
+                />
+                Swims
+            </label>
             <Container maxWidth="md"> 
                 <CanvasJSChart options = {options}
                 />
